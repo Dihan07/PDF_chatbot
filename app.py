728629e610e740f7973bdeb2582c7972ab3623eb
Tanveer Kaiser <dihankt@gmail.com>
2025-08-30 03:16:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 import shutil
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
@@ -224,4 +224,5 @@ def main():
 
 
 if __name__ == "__main__":
+
     main()
